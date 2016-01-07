670c187c5787657f99df412ce85ecfe320d5fef3
David Mann <stratlater@gmail.com>
2016-01-07 17:33:16 -0600

    Palette selection using touch.

--- palette-canvas.js
+++ palette-canvas.js
@@ -4,12 +4,13 @@
   exports.instance = function(opts){
     var $canvas = opts.$canvas;
     this.swatches = opts.swatches;
+    this.curSwatchName = null;
     
     this.swatchSize = 40;
     this.swatchPadding = 8;
     
     this.origin = [8.5,8.5]; // 8.5, 8.5 TODO center palette
-    this.touches = {};
+    this.touchId = -1;
     
     // - Size & setup drawing environment ---
     this.canvas = $canvas.get(0);
@@ -24,117 +25,116 @@
     
     // - Bind listeners ---
     
-    // this.canvas.addEventListener('touchstart', this);
-    // this.canvas.addEventListener('touchend', this);
-    // this.canvas.addEventListener('touchcancel', this);
-    // this.canvas.addEventListener('touchmove', this);
+    this.canvas.addEventListener('touchstart', this);
+    this.canvas.addEventListener('touchend', this);
+    this.canvas.addEventListener('touchcancel', this);
+    this.canvas.addEventListener('touchmove', this);
     
-    this.drawSwatchGrid(this.swatches, this.origin[0], this.origin[1]);
+    this.drawSwatchGrid();
+  };
+  
+  exports.instance.prototype.swatchIdxOfCoords = function(x, y){
+    var regionSize = this.swatchSize + this.swatchPadding;
+    var minX, maxX;
+    
+    var minY = this.origin[1];
+    var maxY = this.origin[1] + regionSize;
+    
+    if(y < minY || maxY <= y) // no further checks needed if not on same row as swatches
+      return -1;
+    
+    for(var regionI=0; regionI < Object.keys(this.swatches).length; regionI++){
+      minX = this.origin[0] + regionI * regionSize;
+      maxX = this.origin[0] + (regionI+1) * regionSize;
+    
+      if(minX <= x && x < maxX)
+        return regionI;
+    }
+    return -1;
   };
   
   exports.instance.prototype.handleEvent = function(evt){
     evt.preventDefault();
     
-    // draw brush underneath other elements already drawn
-    this.ctx.globalCompositeOperation = 'destination-over';
-    
     var curTouch = evt.changedTouches;
+    var curSwatchIdx = -1;
     var canvasClientRect = this.canvas.getBoundingClientRect();
-        
+    
     switch(evt.type){
       case 'touchstart':
         for(var touchI=0; touchI < curTouch.length; touchI++){
-          this.touches[curTouch[touchI].identifier] = {
-            remainLength: 0, // remaining length in stroke not covered by stamps
-            pageX:        curTouch[touchI].pageX,
-            pageY:        curTouch[touchI].pageY,
-            clientX:      curTouch[touchI].clientX,
-            clientY:      curTouch[touchI].clientY
-          };
-          
           var relX = curTouch[touchI].clientX - canvasClientRect.left;
           var relY = curTouch[touchI].clientY - canvasClientRect.top;
           
-          this.stampBrush(relX, relY);
+          // if position is inside a swatch, use this ID and no others
+          curSwatchIdx = this.swatchIdxOfCoords(relX, relY);
+          
+          if(curSwatchIdx !== -1){
+            this.touchId = curTouch[touchI].identifier;
+            this.setSwatch(Object.keys(this.swatches)[curSwatchIdx]);
+            break;
+          }
         }
         break;
       case 'touchmove':
         for(var touchI=0; touchI < curTouch.length; touchI++){
           var foundId = curTouch[touchI].identifier;
-          if(!this.touches[foundId]){
-            console.log('Unable to continue touch!');
+          if(foundId !== this.touchId) // only continue the touch that fell inside a swatch region
             continue;
-          }
           
-          var startX = this.touches[foundId].clientX - canvasClientRect.left;
-          var startY = this.touches[foundId].clientY - canvasClientRect.top;
+          var relX = curTouch[touchI].clientX - canvasClientRect.left;
+          var relY = curTouch[touchI].clientY - canvasClientRect.top;
+                    
+          curSwatchIdx = this.swatchIdxOfCoords(relX, relY);
           
-          var endX = curTouch[touchI].clientX - canvasClientRect.left;
-          var endY = curTouch[touchI].clientY - canvasClientRect.top;
-          
-          var remainLength = this.stampSegment([startX, startY], [endX, endY], this.brushSpacing, this.touches[foundId].remainLength);
-          
-          this.touches[foundId] = { // replace the touch stored for the ID
-            remainLength: remainLength,
-            pageX:        curTouch[touchI].pageX,
-            pageY:        curTouch[touchI].pageY,
-            clientX:      curTouch[touchI].clientX,
-            clientY:      curTouch[touchI].clientY
-          };
+          if(curSwatchIdx !== -1)
+            this.setSwatch(Object.keys(this.swatches)[curSwatchIdx]);
         }
         break;
       case 'touchend':
-        for(var touchI=0; touchI < curTouch.length; touchI++){
-          var foundId = curTouch[touchI].identifier;
-          if(!this.touches[foundId]){
-            console.log('Unable to find touch to end!');
-            continue;
-          }
-          
-          var startX = this.touches[foundId].clientX - canvasClientRect.left;
-          var startY = this.touches[foundId].clientY - canvasClientRect.top;
-          
-          var endX = curTouch[touchI].clientX - canvasClientRect.left;
-          var endY = curTouch[touchI].clientY - canvasClientRect.top;
-          
-          this.stampSegment([startX, startY], [endX, endY], this.brushSpacing, this.touches[foundId].remainLength);
-          
-          delete this.touches[foundId]; // remove the stored touch
-        }
-        
-        break;
       case 'touchcancel':
         for(var touchI=0; touchI < curTouch.length; touchI++){
           var foundId = curTouch[touchI].identifier;
-          if(!this.touches[foundId]){
-            console.log('Unable to find touch to cancel!');
+          if(foundId !== this.touchId) // only continue the touch that fell inside a swatch region
             continue;
-          }
           
-          delete this.touches[foundId];
+          var relX = curTouch[touchI].clientX - canvasClientRect.left;
+          var relY = curTouch[touchI].clientY - canvasClientRect.top;
+                    
+          curSwatchIdx = this.swatchIdxOfCoords(relX, relY);
+          
+          if(curSwatchIdx !== -1)
+            this.setSwatch(Object.keys(this.swatches)[curSwatchIdx]);
+          
+          this.touchId = -1;
         }
-        
         break;
     }
-    
-    this.ctx.globalCompositeOperation = 'source-over';
   };
       
   // - Public API ---
   
-  exports.instance.prototype.drawSwatchGrid = function(swatches, x, y){
+  exports.instance.prototype.drawSwatchGrid = function(){
     this.ctx.save();
     this.ctx.lineWidth = 1;
     this.ctx.strokeStyle = 'black';
     
-    var curPos = {x: x, y: y};
+    var curPos = {x: this.origin[0], y: this.origin[1]};
     
     var swatchI=0;
-    for(var swatch in swatches){
-      this.ctx.fillStyle = swatches[swatch];
-      this.ctx.fillRect(curPos.x, curPos.y, this.swatchSize, this.swatchSize);
+    var curSwatchSize = this.swatchSize;
+    
+    for(var swatch in this.swatches){
+      this.ctx.fillStyle = this.swatches[swatch];
       
-      this.ctx.strokeRect(curPos.x, curPos.y, this.swatchSize, this.swatchSize);
+      // Make the currently-selected swatch, if any, larger than the rest
+      if(this.curSwatchName && swatch === this.curSwatchName)
+        curSwatchSize = this.swatchSize+(this.swatchPadding/2);
+      else
+        curSwatchSize = this.swatchSize;
+      
+      this.ctx.fillRect(curPos.x, curPos.y, curSwatchSize, curSwatchSize);
+      this.ctx.strokeRect(curPos.x, curPos.y, curSwatchSize, curSwatchSize);
       
       curPos.x += this.swatchSize + this.swatchPadding;
      
@@ -144,13 +144,18 @@
     this.ctx.restore();
   };
           
-  exports.instance.prototype.changeColor = function(callback){
-    this.colorCallback = callback.bind(this);
+  exports.instance.prototype.changeSwatch = function(callback){
+    this.swatchCallback = callback.bind(this);
   };
   
-  exports.instance.prototype.setColor = function(color){
-    if(this.colorCallback)
-      this.colorCallback(color);
+  exports.instance.prototype.setSwatch = function(swatchName){
+    this.curSwatchName = swatchName;
+    
+    this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+    this.drawSwatchGrid();
+    
+    if(this.swatchCallback)
+      this.swatchCallback(this.swatches[this.curSwatchName]);
   };
   
 })(jQuery, window.PaletteCanvas = {});
